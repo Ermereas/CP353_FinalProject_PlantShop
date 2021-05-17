bc92b5c33d1805ccccb1f2a40f464f6cbd97474a
Manapon Sanguanklin <73510481+Ermereas@users.noreply.github.com>
2021-05-17 11:12:44 +0700

    Delete Final Project/flask-project directory

--- Final Project/flask-project/api/routes.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Api
-
-from api.plant import PlantAPI,PlantDelAPI
-
-def create_route(api: Api):
-    api.add_resource(PlantAPI, '/plant')
-    api.add_resource(PlantDelAPI, '/plant/delete/<name>')
